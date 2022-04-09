d2e1e18c42f4ec58de01b18f720617c5cefd6378
Qasim Sardar <qasimsardar7203@gmail.com>
2022-04-09 20:17:40 +0500

    function compute()

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+
+    let SI = (p*r*y)/100;
+    let total = +SI + +p;
+    let fYear = +2022 + +y ;
+
+
+    document.getElementById("results").innerHTML="If you deposit " + p + "$, at an interest rate of " + r + "% you'll receive " + total + "$ in the year " + fYear;
+
 }
-        
+
+function rangeValueDisplay(){
+    r = document.getElementById("rate").value;
+    document.getElementById("rateDisplay").innerHTML= r + "%";
+}   
